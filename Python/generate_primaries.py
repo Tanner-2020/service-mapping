8c7d2274c775fbb97c66df595e2fc3396b36b0c0
Tanner2020 <tanjhall2020@gmail.com>
2024-02-08 10:25:43 -0600

    Implemented Database Connection
    Added database connection parameters through .env file.
    Created initial test query to pull data from Sql database.

--- Python/generate_primaries.py
+++ Python/generate_primaries.py
@@ -1,9 +1,30 @@
+import os
+import warnings
 import mysql.connector
+import pandas as pd
+from dotenv import load_dotenv
 
 def main():
-    print("Creating service location primary servicers.")
-    # TODO: For each service location, select all service events that occurred during the last 3 years from date.
-    # Save data to a new database table. 
+
+    warnings.filterwarnings('ignore')
+    load_dotenv()
+    try:
+        # Connection to database
+        cnx = mysql.connector.connect(user=os.getenv('user'), password=os.getenv('password'),
+                                      host=os.getenv('host'), database=os.getenv('database'))
+        print("Successfully connected to " + os.getenv('database') + ".")
+
+        # Query of database test data
+        query = """SELECT * FROM test_data"""
+        test_df = pd.read_sql(query, cnx)
+        test_df = test_df.set_index('data_id')
+        print(test_df)
+        
+        # Close database connection
+        cnx.close()
+        print("Successfully closed database connection to " + os.getenv('database') + ".")
+    except Exception as e:
+        print("Failed to connect to Sql database. Exception: " + e)
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 A python program to process data from an SQL database to identify an empolyee responsible for a particular zip code.
 
 ## Required Libraries
+- `dotenv` is used to load environment variables for connection to the mySql database server.
 - `mysql-connector` for use with mySQL database connections.
+- `pandas` for table storage and operation executions.
