fe1cd4d8fa24458a3ba04355b22e1b6dee8e864c
Tanner2020 <tanjhall2020@gmail.com>
2024-02-16 14:10:15 -0600

    Implemented basic plotting
    Created plot using geopandas and matplotlib.
    Displayed zip code boarders on output.

--- Python/display_map.py
+++ Python/display_map.py
@@ -1,4 +1,11 @@
-import geopandas
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def show_map():
-    minnesota = geopandas.read_file('../shpefiles/')
+    minnesota = gpd.read_file('./shapefiles/shp_bdry_zip_code_tabulation_areas/zip_code_tabulation_areas.shp')
+    minnesota = minnesota.to_crs("EPSG:3395")
+    minnesota.boundary.plot()
+    plt.show()
+
+show_map()
