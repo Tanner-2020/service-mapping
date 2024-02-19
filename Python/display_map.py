70650d0f0c71438fb67aae70d2dd096448b8ffb4
Tanner2020 <tanjhall2020@gmail.com>
2024-02-19 14:41:27 -0600

    Interactive Mapping
    Implemented interactive mapping with geoDataFrame.explore().
    Created path to save html file of the map.
    Opened resulting file in a web browser using the built-in web browser utility.

--- Python/display_map.py
+++ Python/display_map.py
@@ -1,11 +1,13 @@
 import geopandas as gpd
-import matplotlib.pyplot as plt
 import pandas as pd
+import webbrowser
 
 def show_map():
     minnesota = gpd.read_file('./shapefiles/shp_bdry_zip_code_tabulation_areas/zip_code_tabulation_areas.shp')
     minnesota = minnesota.to_crs("EPSG:3395")
-    minnesota.boundary.plot()
-    plt.show()
+    map = minnesota.explore()
+    output_path = r"./mn_zip.html"
+    map.save(output_path)
+    webbrowser.open('mn_zip.html')
 
 show_map()
