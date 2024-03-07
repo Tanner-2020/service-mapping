7751e973bc15a292c1d2ba3cf954046f3654c07d
Tanner2020 <tanjhall2020@gmail.com>
2024-03-07 08:34:08 -0600

    Created button to open map location.
    Created a button to open the map in a new tab.
    Added styling to the button for ease of use.
    Note: File referencing must be resolved first before functionality is complete

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <div className='Main-content'>
           <div id='Map-box'>
             <Iframe title="Map display"></Iframe>
+            <a id='Map-button' href='' target='_blank'>Open map in new tab</a>
           </div>
           <div id='Search-viewer'>
             {searchModule()}
