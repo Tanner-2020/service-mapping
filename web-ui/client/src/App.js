9fd332ca5c761a6e3aafae9793564b95fbbfcfa5
Tanner2020 <tanjhall2020@gmail.com>
2024-03-26 08:50:09 -0500

    Implemented zip code search
    Restructured the location of the search module into App.js for easier search data updating.
    Implemented the ability to search for zip codes in the Search.js file.

--- web-ui/client/src/App.js
+++ web-ui/client/src/App.js
@@ -1,18 +1,20 @@
 import Iframe from 'react-iframe'
 import './App.css';
-import { searchModule } from './Search';
+import { getSearchResults } from './Search';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
   const [servicers, setServicers] = useState([])
+  const [searchList, setSearchList] = useState([])
 
   useEffect(()=>{
     const fetchAllServicers = async () => {
       try{
         const res = await axios.get(process.env.REACT_APP_CONNPORT);
         setServicers(res.data);
+        setSearchList(res.data);
       }catch(err){
         console.log(err);
       }
@@ -30,7 +32,24 @@ function App() {
             <a id='Map-button' href='' target='_blank'>Open map in new tab</a>
           </div>
           <div id='Search-viewer'>
-            {searchModule(servicers)}
+            <div>
+              <h3 id="Search-header">Location Search</h3>
+              <input type="text" id="Search-text" name="search-text" placeholder="Enter zip code, city, or service center" onChange={(e) => setSearchList(getSearchResults(e.target.value, servicers))}></input>
+              <div>
+                  <h6 id="Search-result">Results</h6>
+              </div>
+              <div id='Results'>
+                  {searchList.map(zip=>(
+                      <div className='Result-item' key={zip.zip_code}>
+                          <h6 className='Headline'>{zip.zip_code}</h6>
+                          <p className='Primary'><b>Primary:</b> {zip.primary_serv}</p>
+                          <p className='Secondary'><b>Secondary:</b> {zip.secondary_serv}</p>
+                          <p className='Tertiary'><b>Tertiary:</b> {zip.tertiary_serv}</p>
+                          <a className='DataToggle'>Show More</a>
+                      </div>
+                  ))}
+              </div>
+            </div>
           </div>
         </div>
       </header>
--- web-ui/client/src/Search.js
+++ web-ui/client/src/Search.js
@@ -1,53 +1,17 @@
-export function searchModule(servicers) {
-
-    return (
-        <div>
-            <h3 id="Search-header">Location Search</h3>
-            <input type="text" id="Search-text" name="search-text" placeholder="Enter zip code, city, or service center" onChange={(e) => getSearchResults(e.target.value)}></input>
-            <div>
-                <h6 id="Search-result">Results</h6>
-            </div>
-            <div id='Results'>
-                {servicers.map(zip=>(
-                    <div className='Result-item' key={zip.zip_code}>
-                        <h6 className='Headline'>{zip.zip_code}</h6>
-                        <p className='Primary'><b>Primary:</b> {zip.primary_serv}</p>
-                        <p className='Secondary'><b>Secondary:</b> {zip.secondary_serv}</p>
-                        <p className='Tertiary'><b>Tertiary:</b> {zip.tertiary_serv}</p>
-                        <a className='DataToggle'>Show More</a>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
+export function getSearchResults(search_term, data_list) {
+    console.log("getSearchResults")
+    var zip_res = getZipSearch(search_term, data_list);
+    return zip_res;
 }
 
-
-function getSearchResults(test_value) {
-    var zip_res = getZipSearch(test_value);
-    var city_res = getCitySearch(test_value);
-    var facility_res = getFacilitySearch(test_value);
-    var search_res = [zip_res, city_res, facility_res];
-
-    // TODO: Create function to erase and append results to cells in result list.
-}
-
-function getZipSearch(zip_code) {
-    // TODO: Query database with following query: Select * from zip_codes left join servicer_data where zip_code contains zip_code
-    var data = ['Test', 'Values', 'Here'];
-    return data;
-}
-
-
-function getCitySearch(city_name) {
-    // TODO: Query database with following query: Select * from city left join servicer_data where zip_code contains zip_code
-    var data = ['Test', 'Values', 'Here'];
-    return data;
-}
-
-
-function getFacilitySearch(facility_name) {
-    // TODO: Query database with following query: Select * from facility left join servicer_data where zip_code contains zip_code
-    var data = ['Test', 'Values', 'Here'];
+function getZipSearch(search_term, data_list) {
+    var data = [];
+    for(var i = 0; i < data_list.length; i++){
+        console.log(data_list[i].zip_code)
+        if(String(data_list[i].zip_code).includes(String(search_term))) {
+            data.push(data_list[i]);
+        }
+    }
+    console.log(data);
     return data;
 }
